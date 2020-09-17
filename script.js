3721c25c12b477d0a534129cff9abbb271777a48
mariajalmeida <mariajalmeida@users.noreply.github.com>
2020-09-17 20:58:52 +0200

    script - filip

--- script.js
+++ script.js
@@ -12,18 +12,25 @@ function handleData(data) {
     console.log("myData - console");
     console.log(myData);
     myData.forEach(showData);
+
 }
 
 function showData(singleRowData) {
     console.log(singleRowData - console);
     console.log(singleRowData);
 
+    if (singleRowData.gold) {
+        console.log("golden");
+
+    } else {
+        console.log("not Golden");
+    }
 
     const template = document.querySelector('template').content;
     const clone = template.cloneNode(true);
     const h2 = clone.querySelector('h2');
     h2.textContent = singleRowData.gsx$product.$t;
-    
+
     var b = clone.querySelector("img")
     b.setAttribute("src", singleRowData.gsx$images.$t)
 
@@ -31,18 +38,11 @@ function showData(singleRowData) {
     h4_span.textContent = singleRowData.gsx$price.$t;
 
     document.querySelector('main').appendChild(clone);
-    
-        if (data.gold) {
-        console.log("golden");
 
-        }
-        else{
-        console.log("not Golden");
-        }
-      
-    
 
-    /* Ture/false method
+}
+
+/* Ture/false method
     
     if (myData.gold == 'TRUE') {
         console.log("golden");
@@ -51,8 +51,7 @@ function showData(singleRowData) {
     }   
 */
 
-
-  /* Forloop method test with p.textcontent  
+/* Forloop method test with p.textcontent
     let divs = document.getElementsByClassName('product');
 
     for (let x = 0; x < divs.length; x++) {
@@ -63,7 +62,4 @@ function showData(singleRowData) {
           div.style.display = 'none';
       }
     }
-  */  
-
-
-
+  */
